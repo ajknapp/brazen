f5654356bda819b12ebddab85ac89d1b10e16b79
Andrew Knapp <andy.j.knapp@gmail.com>
2024-07-23 20:18:32 -0700

    Fix uniformT to do what it says on the tin.

--- src/Brazen/Distributions.hs
+++ src/Brazen/Distributions.hs
@@ -129,7 +129,7 @@ halfCauchy l = MCLMC $ proc (scale, hmc) -> do
   returnA -< (phi, hmc')
 
 uniformT :: (Floating a) => V3 a -> a
-uniformT (V3 a' b' v) = let c = b' - a' in negate . log $ c / (pi * (1 + v * v))
+uniformT (V3 a b v) = ((b - a) / pi) * (atan v + 0.5 * pi) + a
 
 uniform ::
   (CmdRAD m e a, Floating (ADExp e a), Eq a, Floating a, ExpInject e a) =>
@@ -143,8 +143,8 @@ uniform a b l = MCLMC $ proc (_, hmc) -> do
   v <- opNAD uniformT -< V3 (auto a) (auto b) theta
   updateLP -< (hmc, v, lp)
 
-cauchyD :: Floating a => V3 a -> a
-cauchyD (V3 mu sigma2 x) = let y = x - mu in log1p (y*y/sigma2)
+cauchyD :: (Floating a) => V3 a -> a
+cauchyD (V3 mu sigma2 x) = let y = x - mu in log1p (y * y / sigma2)
 
 cauchy ::
   (CmdRAD m e a, Floating (ADExp e a), Floating a, Eq a, ExpInject e a) =>
@@ -185,8 +185,8 @@ logcauchy' l = MCLMC $ proc ((mu, sigma2), hmc) -> do
   phi <- opNAD (\(V3 m s t) -> exp $ m + sqrt s * t) -< V3 mu sigma2 theta
   updateLP -< (hmc, phi, lp)
 
-exponentialD :: Floating a => V2 a -> a
-exponentialD (V2 lam x) = lam*exp x - x - log lam
+exponentialD :: (Floating a) => V2 a -> a
+exponentialD (V2 lam x) = lam * exp x - x - log lam
 
 exponential ::
   (CmdRAD m e a, Floating (ADExp e a), Floating a, Eq a, ExpInject e a) =>
@@ -205,10 +205,10 @@ exponential' ::
 exponential' l = MCLMC $ proc (lam, hmc) -> do
   let theta = hmc ^. hmcState . l
   lp <- opNAD exponentialD -< V2 (auto 1) theta
-  phi <- opNAD (\(V2 lam' t) -> lam'*t) -< V2 lam theta
+  phi <- opNAD (\(V2 lam' t) -> lam' * t) -< V2 lam theta
   updateLP -< (hmc, phi, lp)
 
-laplaceD :: Floating a => V3 a -> a
+laplaceD :: (Floating a) => V3 a -> a
 laplaceD (V3 mu sigma x) = let y = x - mu in abs y / sigma + log sigma + log 2
 
 laplace ::
